37b2eded5205b132594fb9b41a2f793c592c7420
Aleksander Piotrowski <pelotasplus@gmail.com>
2020-07-14 13:32:47 +0200

    RxJava test

--- app/src/main/java/com/example/nasaneo/data/NeoRepository.kt
+++ app/src/main/java/com/example/nasaneo/data/NeoRepository.kt
@@ -1,12 +1,11 @@
 package com.example.nasaneo.data
 
 import io.reactivex.Single
-import com.example.nasaneo.data.model.Feed
 import javax.inject.Inject
 
 class NeoRepository @Inject constructor(
     private val nasaNeoApi: NasaNeoApi
 ) {
-    fun getFeed(): Single<Feed> =
-        nasaNeoApi.getFeed()
+    fun getFeed(): Single<Unit> =
+        Single.never()
 }
--- app/src/main/java/com/example/nasaneo/features/list/ListViewModel.kt
+++ app/src/main/java/com/example/nasaneo/features/list/ListViewModel.kt
@@ -2,21 +2,12 @@ package com.example.nasaneo.features.list
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import io.reactivex.Scheduler
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.rxkotlin.addTo
-import io.reactivex.rxkotlin.subscribeBy
-import com.example.nasaneo.di.AppModule.IO_SCHEDULER
-import com.example.nasaneo.di.AppModule.UI_SCHEDULER
 import com.example.nasaneo.domain.model.Event
-import com.example.nasaneo.domain.usecase.GetFeedUseCase
+import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Inject
-import javax.inject.Named
 
 class ListViewModel @Inject constructor(
-    getFeedUseCase: GetFeedUseCase,
-    @Named(IO_SCHEDULER) ioScheduler: Scheduler,
-    @Named(UI_SCHEDULER) uiScheduler: Scheduler
+    // feel free to inject required dependencies here
 ) : ViewModel() {
 
     private val compositeDisposable = CompositeDisposable()
@@ -25,24 +16,7 @@ class ListViewModel @Inject constructor(
     val viewState = MutableLiveData<ListViewState>()
 
     init {
-        getFeedUseCase.build()
-            .map { items ->
-                items.map { item ->
-                    ListItemState(
-                        neo = item,
-                        name = item.name,
-                        url = item.url
-                    )
-                }
-            }
-            .subscribeOn(ioScheduler)
-            .observeOn(uiScheduler)
-            .subscribeBy(
-                onSuccess = {
-                    viewState.value = ListViewState(items = it)
-                }
-            )
-            .addTo(compositeDisposable)
+        // put your code fetching data here
     }
 
     fun onItemClicked(itemState: ListItemState) {
