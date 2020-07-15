a05e55af17420196b9dd9106932d27e38167ceee
Sviatoslav Kostenko <sviatoslav.kostenko@trinetix.com>
2020-07-15 21:43:38 +0300

    Implement rxjava

--- app/src/main/java/com/example/nasaneo/data/NeoRepository.kt
+++ app/src/main/java/com/example/nasaneo/data/NeoRepository.kt
@@ -1,11 +1,11 @@
 package com.example.nasaneo.data
 
+import com.example.nasaneo.data.model.Feed
 import io.reactivex.Single
 import javax.inject.Inject
 
 class NeoRepository @Inject constructor(
     private val nasaNeoApi: NasaNeoApi
 ) {
-    fun getFeed(): Single<Unit> =
-        Single.never()
+    fun getFeed(): Single<Feed> = nasaNeoApi.getFeed()
 }
--- app/src/main/java/com/example/nasaneo/features/list/ListViewModel.kt
+++ app/src/main/java/com/example/nasaneo/features/list/ListViewModel.kt
@@ -2,21 +2,38 @@ package com.example.nasaneo.features.list
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.nasaneo.data.NeoRepository
+import com.example.nasaneo.data.model.Feed
 import com.example.nasaneo.domain.model.Event
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class ListViewModel @Inject constructor(
-    // feel free to inject required dependencies here
+    neoRepository: NeoRepository
 ) : ViewModel() {
 
     private val compositeDisposable = CompositeDisposable()
 
     val itemToOpen = MutableLiveData<Event<ListItemState>>()
+
     val viewState = MutableLiveData<ListViewState>()
 
     init {
-        // put your code fetching data here
+        val disposable = neoRepository
+            .getFeed()
+            .map { feed: Feed ->
+                feed.nearEarthObjects.values.first()
+            }
+            .map { items -> items.map { item -> ListItemState(item, item.name, item.url) } }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { listItemStates ->
+                viewState.value = ListViewState(listItemStates)
+            }
+
+        compositeDisposable.add(disposable)
     }
 
     fun onItemClicked(itemState: ListItemState) {
